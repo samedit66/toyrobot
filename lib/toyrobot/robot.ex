c8abcc94109cf18505b4d479a5098ab9d3e3ff13
samedit66 <samedit66@yandex.ru>
2025-08-19 22:44:41 +0300

    feat(ToyRobot.Robot): add move_north, move_south

--- lib/toyrobot/robot.ex
+++ lib/toyrobot/robot.ex
@@ -34,4 +34,40 @@ defmodule ToyRobot.Robot do
   def move_west(robot) do
     %{east: robot.east - 1}
   end
+
+  @doc"""
+  Moves robot south one space.
+
+  ## Examples
+
+    iex> alias ToyRobot.Robot
+    ToyRobot.Robot
+    iex> robot = %{north: 0}
+    %{north: 0}
+    iex> robot |> Robot.move_north
+    %{north: 1}
+    iex> robot |> Robot.move_north |> Robot.move_north |> Robot.move_north
+    %{north: 3}
+  """
+  def move_north(robot) do
+    %{north: robot.north + 1}
+  end
+
+  @doc"""
+  Moves robot south one space.
+
+  ## Examples
+
+    iex> alias ToyRobot.Robot
+    ToyRobot.Robot
+    iex> robot = %{north: 0}
+    %{north: 0}
+    iex> robot |> Robot.move_south
+    %{north: -1}
+    iex> robot |> Robot.move_south |> Robot.move_south |> Robot.move_south
+    %{north: -3}
+  """
+  def move_south(robot) do
+    %{north: robot.north - 1}
+  end
 end
