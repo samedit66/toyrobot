62225c0b7930e7c3ca14d165cef05f926ed22a53
samedit66 <samedit66@yandex.ru>
2025-08-19 22:35:23 +0300

    feat(ToyRobot): split up move into move_west and move_east

--- lib/toyrobot/robot.ex
+++ lib/toyrobot/robot.ex
@@ -1,6 +1,6 @@
 defmodule ToyRobot.Robot do
   @doc """
-  Moves the robot forward one space.
+  Moves the robot east one space.
 
   ## Examples
 
@@ -8,12 +8,30 @@ defmodule ToyRobot.Robot do
     ToyRobot.Robot
     iex> robot = %{position: 0}
     %{position: 0}
-    iex> robot |> Robot.move
+    iex> robot |> Robot.move_east
     %{position: 1}
-    iex> robot |> Robot.move |> Robot.move |> Robot.move
+    iex> robot |> Robot.move_east |> Robot.move_east |> Robot.move_east
     %{position: 3}
   """
-  def move(robot) do
+  def move_east(robot) do
     %{position: robot.position + 1}
+  end
+
+  @doc"""
+  Moves the robot west one space.
+
+  ## Examples
+
+    iex> alias ToyRobot.Robot
+    ToyRobot.Robot
+    iex> robot = %{position: 0}
+    %{position: 0}
+    iex> robot |> Robot.move_west
+    %{position: -1}
+    iex> robot |> Robot.move_west |> Robot.move_west |> Robot.move_west
+    %{position: -3}
+  """
+  def move_west(robot) do
+    %{position: robot.position - 1}
   end
 end
